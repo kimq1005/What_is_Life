ef15d0da6a790c5ded5ef8169ac571bcea5d0b7f
SeoungHyeon Kim <68366753+kimq1005@users.noreply.github.com>
2022-03-30 17:16:12 +0900

    [gradle 및 color ]

--- app/src/main/java/test/map/whatislife/MainActivity.kt
+++ app/src/main/java/test/map/whatislife/MainActivity.kt
@@ -2,10 +2,18 @@ package test.map.whatislife
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import test.map.whatislife.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private var mainBinding: ActivityMainBinding? = null
+    private val binding get() = mainBinding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
     }
 }
